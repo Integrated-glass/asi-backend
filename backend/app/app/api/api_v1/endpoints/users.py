def3c528cabfeee9ffaffd8db3bc2eb867d0e09b
WinnerOK <danman339@gmail.com>
2019-08-24 19:30:55 +0300

    commented unused user router

--- backend/app/app/api/api_v1/endpoints/users.py
+++ backend/app/app/api/api_v1/endpoints/users.py
@@ -1,15 +1,15 @@
 from typing import List
 
 from fastapi import APIRouter, Body, Depends, HTTPException
-from fastapi.encoders import jsonable_encoder
-from pydantic.types import EmailStr
-from sqlalchemy.orm import Session
-
-from app import crud
-from app.api.utils.db import get_db
-from app.api.utils.security import get_current_active_superuser, get_current_active_user
-from app.core import config
-from app.db_models.user import User as DBUser
+# from fastapi.encoders import jsonable_encoder
+# from pydantic.types import EmailStr
+# from sqlalchemy.orm import Session
+#
+# from app import crud
+# from app.api.utils.db import get_db
+# from app.api.utils.security import get_current_active_superuser, get_current_active_user
+# from app.core import config
+# from app.db_models.user import User as DBUser
 #from app.models.user import User, UserCreate, UserInDB, UserUpdate
 
 router = APIRouter()
