94a15596e4c15a692c114266934fcb295768c3a5
WinnerOK <danman339@gmail.com>
2019-08-24 19:29:23 +0300

    commented unused code

--- backend/app/app/api/api_v1/endpoints/items.py
+++ backend/app/app/api/api_v1/endpoints/items.py
@@ -3,10 +3,10 @@ from typing import List
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
-from app import crud
-from app.api.utils.db import get_db
-from app.api.utils.security import get_current_active_user
-from app.db_models.user import User as DBUser
+# from app import crud
+# from app.api.utils.db import get_db
+# from app.api.utils.security import get_current_active_user
+# from app.db_models.user import User as DBUser
 #from app.models.item import Item, ItemCreate, ItemUpdate
 
 router = APIRouter()
