4e891575b3d4e2c9c8ef31bc206d28899dbb96ce
WinnerOK <danman339@gmail.com>
2019-08-24 20:19:21 +0300

    enum for roles

--- backend/app/app/db_models/user.py
+++ backend/app/app/db_models/user.py
@@ -1,15 +1,21 @@
-from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy import Enum, Column, Integer, String
 from sqlalchemy.orm import relationship
 
 from app.db.base_class import Base
+import enum
+
+
+class Roles(str, enum.Enum):
+    entrepreneur = "entrepreneur"
+    investor = "investor"
 
 
 class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
-    role = Column(String, nullable=False)
+    role = Column(Enum(Roles), nullable=False)
 
     passport = relationship("Passport", back_populates="user", uselist=False)
-    entrepreneur = relationship("Entrepreneur", back_populates="user",  uselist=False)
+    entrepreneur = relationship("Entrepreneur", back_populates="user", uselist=False)
     investor = relationship("Investor", back_populates="user", uselist=False)
